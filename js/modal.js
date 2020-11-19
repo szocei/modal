85dcda88bdeee335c13c311692f5031910fa5398
szocei <szocei.zoltan@gmail.com>
2020-11-19 11:53:43 +0100

    11.19

---
+++ js/modal.js
@@ -0,0 +1,9 @@
+function showModal() {
+    document.querySelector('.modal').classList.add('show__modal');
+  }
+  function closeModal() {
+    document.querySelector('.modal').classList.add('close__modal');
+    window.location.reload();
+  }
+
+  
