2ba817144e951628ae5ab8c1a9ec003f9d0f2734
szocei <szocei.zoltan@gmail.com>
2020-11-19 12:37:29 +0100

    11.19__2

--- js/modal.js
+++ js/modal.js
@@ -1,9 +1,12 @@
 function showModal() {
+    document.querySelector('.overlay').style.display = "inline";
     document.querySelector('.modal').classList.add('show__modal');
+    
+    
+    
   }
   function closeModal() {
     document.querySelector('.modal').classList.add('close__modal');
     window.location.reload();
+    document.querySelector('.overlay').style.display = "none";
   }
-
-  
